7154fec8b6f42bb93c12b8cf9b4ddff9cc7c78e2
Anthony Wynne <12174019+ant358@users.noreply.github.com>
2021-05-31 13:43:30 +0100

    moved the thread timer to another file

---
+++ tests/test_timer.py
@@ -0,0 +1,12 @@
+# from utils import timer
+# import time
+
+
+# def test_repeat_function_call(capsys):
+#     x = timer.RepeatFunctionCall(interval=1)
+#     x.start()
+#     time.sleep(1)
+#     captured = capsys.readouterr()
+#     x.stop()
+#     message = "RepeatFunctionCall failed to start"
+#     assert 'call' in captured, message
